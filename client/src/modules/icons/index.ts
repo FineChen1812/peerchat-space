c88744226c93cbc8cc79cd4c3c0f712b4cf61213
FineChen <1622920569@qq.com>
2024-09-03 23:12:33 +0800

    💄起始页样式调整

--- client/src/modules/icons/index.ts
+++ client/src/modules/icons/index.ts
@@ -1,48 +1,16 @@
 import type { App } from 'vue'
 import {
-  Close,
-  DoubleLeft,
-  DoubleRight,
-  Github,
-  Left,
-  More,
-  Music,
-  MusicCd,
-  MusicMenu,
-  MusicRhythm,
-  Play,
-  PlayOne,
-  Right,
-  Search,
-  Theme,
-  User,
-  Video,
-  VideoTwo,
+  PhoneVideoCall,
+  Add
 } from '@icon-park/vue-next'
 
 export interface Icons {
-  [key: string]: typeof Search
+  [key: string]: typeof PhoneVideoCall
 }
 
 export const icons: Icons = {
-  IconSearch: Search,
-  IconTheme: Theme,
-  IconGithub: Github,
-  IconLeft: Left,
-  IconRight: Right,
-  IconUser: User,
-  IconMusicCd: MusicCd,
-  IconMusicMenu: MusicMenu,
-  IconMusic: Music,
-  IconVideo: Video,
-  IconClose: Close,
-  IconPlayOne: PlayOne,
-  IconDoubleLeft: DoubleLeft,
-  IconDoubleRight: DoubleRight,
-  IconMore: More,
-  IconVideoTwo: VideoTwo,
-  IconMusicRhythm: MusicRhythm,
-  IconPlay: Play,
+  IconPhoneVideoCall: PhoneVideoCall,
+  IconAdd: Add,
 }
 export const install = (app: App) => {
   for (const key of Object.keys(icons))
--- client/src/views/Home/index.vue
+++ client/src/views/Home/index.vue
@@ -1,5 +1,19 @@
 <script lang="ts" setup>
 import { SettingOutlined } from '@ant-design/icons-vue'
+import { getCurrentDateTime } from '~/utils/util'
+
+const time = ref('')
+const date = ref('')
+let timer = null
+
+onMounted(() => {
+  timer = setInterval(() => {
+    const dateTime = getCurrentDateTime()
+    time.value = dateTime.split(' ')[1]
+    date.value = dateTime.split(' ')[0]
+  }, 1000)
+})
+
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center">
@@ -7,20 +21,34 @@ import { SettingOutlined } from '@ant-design/icons-vue'
       <div class="h-full w-[75px] text-center flex p-[20px] pt-[40px] flex-col justify-between">
         <div>
           <!-- 登录 -->
-          <a-avatar class="text-white bg-[#1677ff]">还打</a-avatar>
+          <a-avatar class="text-white bg-[#1677ff]">阿峰</a-avatar>
         </div>
         <div><SettingOutlined /></div>
       </div>
-      <div class="bg-white w-full rounded-[10px] border-t-[#f0f1f5] border-t">
-        <div class="w-[330px] flex">
+      <div class="bg-white w-full rounded-[10px] border-t-[#f0f1f5] border-t flex items-center">
+        <div class="w-[330px] flex justify-center items-center h-full">
           <div>
-            
+            <div class="w-[72px] cursor-pointer h-[72px] flex flex-wrap justify-center items-center bg-[#1677ff] rounded-2xl">
+              <IconPhoneVideoCall theme="filled" size="48" fill="#fff"/>
+            </div>
+            <div class="text-sm text-[#53576a] text-center mt-4">发起会议</div>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div class="ml-[15px]">
+            <div class="w-[72px] cursor-pointer h-[72px] flex flex-wrap justify-center items-center bg-[#1677ff] rounded-2xl">
+              <IconAdd theme="multi-color" size="48" :fill="['#ffffff' ,'#ffffff' ,'#1677ff' ,'#1677ff']"/>
+            </div>
+            <div class="text-sm text-[#53576a] text-center mt-4">加入会议</div>
+          </div>
         </div>
-        <div></div>
+        <div class="w-[290px] h-[400px] border rounded-[10px]">
+          <div class="w-full h-[72px] py-2 px-4 text-white bg-cover rounded-t-[10px] bg-[url('~/assets/images/meetBg.png')]">
+            <div class="text-2xl">{{ time }}</div>
+            <div class="text-sm">今日 {{ date }}</div>
+          </div>
+          <div>
+            <img class="mt-[57px]" src="../../assets/images/emptyWait.png">
+          </div>
+        </div>
       </div>
     </div>
   </div>
